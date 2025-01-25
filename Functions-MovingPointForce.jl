071d92562e36dc440c6f34f85d954d045bb262e4
Josiah <josiah.s.murray.work@gmail.com>
2025-01-26 10:48:00 +1100

    Added graphing

--- Functions-MovingPointForce.jl
+++ Functions-MovingPointForce.jl
@@ -1,3 +1,4 @@
+using InverseLaplace
 #|||||||||||||||#
 #||--READ ME--||#
 #|||||||||||||||#
@@ -24,8 +25,9 @@ function CalcDynamicDeformation(xVals,tVals, parameters)
 
 
       #Solve for the deformation of the beam at each time and x value.
-      deformation = TalbotMethod(tVal, s-> LaplaceSpaceFunctionMovingPointForce1TZ(x,s,parameters))
-
+      #deformation = real( DirectQuadratureMethod(t, s-> LaplaceSpaceFunctionMovingPointForce1TZ(x,s,parameters)) )
+      ft = InverseLaplace.Weeks( s-> LaplaceSpaceFunctionMovingPointForce1TZ(x,s,parameters), 16)
+      deformation = ft(t)
 
       #Update the deformations matrix.
       deformations[ti,xi] = deformation
@@ -50,9 +52,9 @@ function LaplaceSpaceFunctionMovingPointForce1TZ(x,s, parameters)
   end
   #Always find rj such that it is in the jth quadrant of the complex plane
   ρ = ( abs(r⁴) )^(1/4)
-  theta = mod( atan( imag(r⁴), real(r⁴) ), 2*pi )/4
+  θ = mod( atan( imag(r⁴), real(r⁴) ), 2*pi )/4
 
-  r1 = rho*exp(theta*1im)
+  r1 = ρ*exp(θ*1im)
   r2 = r1*1im
   r3 = -r1
   r4 = -r1*1im
@@ -62,7 +64,7 @@ function LaplaceSpaceFunctionMovingPointForce1TZ(x,s, parameters)
 
   #||--Find b values--||#
   #Solve for the undetermined coefficients by solving a linear system
-  b1,b2,b3,b3 = CoefficientSolverMovingPointForce1TZ(rVals,parameters)
+  b1,b2,b3,b4 = CoefficientSolverMovingPointForce1TZ(s,rVals,parameters)
 
   #||--Construct Laplace-space function--||#
   #Need to choose the correct Laplace-domain function
@@ -76,29 +78,43 @@ function LaplaceSpaceFunctionMovingPointForce1TZ(x,s, parameters)
   end
 
   return ŷ
+
 end
 
 #Solves for the undetermined coefficients
-function CoefficientSolverMovingPointForce1TZ(rVals, parameters)
+function CoefficientSolverMovingPointForce1TZ(s, rVals, parameters)
   EI, m, C0, C1, k0, k1, xtz, P, v = parameters
 
   #LHS matrix for system enforcing continuity conditions.
-  LHSMatrix = zeros(4,4)
+  LHSMatrix = zeros(Complex{Float64}, 4,4)
 
   #RHS Vector for system.
-  RHS = zeros(4)
+  RHS = zeros(Complex{Float64},4)
 
-  for column = 1:4
-    for row  = 1:4
-      if row == 1 || row == 4
+  for row = 1:4
+    for column  = 1:4
+      if column == 1 || column == 4
+        sign = 1
+      else
         sign = -1
       end
-      LHSMatrix[row, column] = sign*rVals[row]^(column-1)*exp(rVals[row]*xtz)
+      LHSMatrix[row, column] = sign*rVals[column]^(row-1)*exp(rVals[column]*xtz)
     end
-    RHS[column] = (-s/v)^(column-1)*( P*exp(-s*xtz/v)/abs(v)  )*(  1/( ( (EI*s^4)/v^4  ) +m*s^2 + C0*s + k0   ) - 1/( ( (EI*s^4)/v^4  ) +m*s^2 + C1*s + k1   )   )
+
+    RHS[row] = (-s/v)^(row-1)*( P*exp(-s*xtz/v)/abs(v)  )*(  1/( ( (EI*s^4)/v^4  ) +m*s^2 + C0*s + k0   ) - 1/( ( (EI*s^4)/v^4  ) +m*s^2 + C1*s + k1   )   )
   end
 
-  bVals = LHSMatrix\RHS
+  try
+    bVals = LHSMatrix\RHS
+  catch
+    println("Error values:")
+    println(s)
+    println(RHS)
+    println(LHSMatrix)
+
+    throw(error("It broke :("))
+  end
+
 
   return bVals
 
@@ -106,4 +122,19 @@ end
 
 function TalbotMethod(tVal, LaplaceFunction)
   return "Not yet implemented." #//TODO Implement Talbot method
+end
+
+#Inverts the Laplace transform using a basic quadrature method.
+function DirectQuadratureMethod(t, LaplaceFunction)
+  sRadius = -10
+  sNum = 1000
+  sStep = 2*sRadius/sNum
+  sVals = 0.01*ones(sNum) + LinRange(-sRadius,sRadius,sNum)*1im
+
+  f = 0
+  for s in sVals
+    f += LaplaceFunction(s)*exp( s*t  )*sStep
+  end
+
+  return f
 end
--- MovingPointForce.jl
+++ MovingPointForce.jl
@@ -7,7 +7,8 @@ This file solves for the dynamic deformation of an infinite Euler-Bernoulli
 beam on a piecewise constant viscoelastic foundation subject to a moving load.
 The model is solved using the method of undetermined coefficients.
 =#
-
+using Plots
+plotlyjs()
 include("Functions-MovingPointForce.jl")
 
 
@@ -17,17 +18,17 @@ include("Functions-MovingPointForce.jl")
 
 #||--Model parameters--||#
 #Things like the beam stiffness, etc.
-EI = 1 #Beam stiffness
-m = 1 #Mass per unit length of beam
-C0 = 1 #Foundation damping before tz
-C1 = 2 #Foundation damping after tz
-k0 = 1 #Foundation stiffness before tz
-k1 = 2 #Foundation stiffness after tz
+EI = 2.3*10^3 #Beam stiffness
+m = 48 #Mass per unit length of beam
+C0 = 10^7 #Foundation damping before tz
+C1 = C0 #Foundation damping after tz
+k0 = 10^7 #Foundation stiffness before tz
+k1 = k0 #Foundation stiffness after tz
 
 xtz = 1 #Position of transition zone
 
-P = 1 #Force conveyed by point load
-v = 1 #Speed of point force
+P = -10^4 #Force conveyed by point load
+v = -5 #Speed of point force
 
 parameters = [EI, m, C0, C1, k0, k1, xtz, P, v]
 
@@ -40,8 +41,8 @@ xNum = 100
 xVals = LinRange(xLeft,xRight,xNum)
 
 tMin = 0
-tMax = 5
-tNum = 101
+tMax = 1
+tNum = 100
 tVals = LinRange(tMin,tMax,tNum)
 
 
@@ -52,8 +53,19 @@ tVals = LinRange(tMin,tMax,tNum)
 
 #Calculate the deformation of the beam for given times and x values
 deformations = CalcDynamicDeformation(xVals,tVals, parameters)
+##
+
+tempDeformations = copy(deformations)
+for i in eachindex(tempDeformations)
+  tempDeformations[i] = max(-0.001, tempDeformations[i])
+  tempDeformations[i] = min(0.001, tempDeformations[i])
+end
 
 
+
+myPlt = plot(xVals, tVals,  deformations, st=:surface)
+display(myPlt)
+println("Finished")
 ##
 
 #||||||||||||||||#
