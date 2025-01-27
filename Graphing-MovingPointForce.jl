0455e2a93f72c8c38ab1dde3cf2ea7e9359a66d3
Josiah <josiah.s.murray.work@gmail.com>
2025-01-27 14:00:43 +1100

    Finalised graphing

--- Graphing-MovingPointForce.jl
+++ Graphing-MovingPointForce.jl
@@ -8,6 +8,15 @@ myPlt = plot(xVals, tVals,  deformations, st=:surface)
 display(myPlt)
 
 #||||----Animation----||||#
+
+
 @gif for ti in 1:tNum
-  p = plot(xVals, deformations[ti,:], ylimits = (-0.01,0.001))
+  p = vline([2],color=RGB(0.1,0.1,0.2), width=3, foreground_color=RGB(0.7,0.7,0.7), background_color=RGB(0.05,0.05,0.1), framestyle=:box)
+  p = vline!([v*(ti-1)*(tMax-tMin)/tNum + xp], width=3, color=RGB(0.3,0.3,0.4))
+  p = plot!(xVals, deformations[ti,:], lc=RGB(0.5,0.4,0.9), lw=3, ylimits = (-0.0075,0.0075), xlimits = (xLeft,xRight))
+  xlabel!("Beam coordinate (m)")
+  ylabel!("Deformation (m)")
 end
+
+
+##
